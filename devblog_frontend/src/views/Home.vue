60074e4061889602e8f08d6ff4476fbc98f42791
Devontae Reid <devdoesit17@gmail.com>
2021-08-03 18:53:09 -0700

    navigation and footer setup

---
+++ devblog_frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <p>This is a paragraph</p>
+    <DevLogo/>
+  </div>
+</template>
+
+<script>
+import DevLogo from "../assets/logo.png";
+export default {
+  name: "Home",
+  components: {
+    DevLogo
+  },
+};
+</script>
